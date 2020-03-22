3ad68ca2a10e653227b4d6612a753250ee6ca043
HGaelW <hirwa.gael@gmail.com>
2020-03-22 20:27:10 +0200

    second commit

---
+++ mathcal.js
@@ -0,0 +1,25 @@
+// This file contains how to do  simple math calculations in Js
+
+// Multiplication
+
+console.log(20*10);
+
+
+
+// addition 
+
+console.log(10+20);
+
+// substraction 
+
+console.log(20-10);
+
+//division
+
+console.log(20/10);
+
+//Finding the remainder, Here we will be using  %  (called modulus)
+
+console.log(20%10);
+
+
