2b5726679162b077668edabb2e4e4129ff8b2b0c
HGaelW <hirwa.gael@gmail.com>
2020-03-22 20:00:14 +0200

    Intro to Js

---
+++ intro.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log("My name is Gael HIRWA");
+console.log("and I'm a newbie in Javascript");
+console.log("and I started #100daysofCode");
+console.log("So that I can learn Js");
+
+
