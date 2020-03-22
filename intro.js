e4fbe8e8e88702a73f0741e226bc51bfccb1c9d3
HGaelW <hirwa.gael@gmail.com>
2020-03-22 20:28:55 +0200

    first comcom

--- intro.js
+++ intro.js
@@ -5,6 +5,6 @@ console.log("Hello World");
 console.log("My name is Gael HIRWA");
 console.log("and I'm a newbie in Javascript");
 console.log("and I started #100daysofCode");
-console.log("So that I can learn Js");
+console.log("So that I can learn Js")
 
 
